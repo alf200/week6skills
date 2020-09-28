f21d18a174b6e0ba7cf78e3eda03f92aa28cd168
Winston Osei-Bonsu <winstonobonsu@gmail.com>
2020-09-27 22:16:23 -0400

    Added logic for anagram checker.

--- anagram.py
+++ anagram.py
@@ -8,3 +8,15 @@ anagram = f"{word_one} and {word_two} are anagrams!"
 not_anagram = f"{word_one} and {word_two} are not anagrams!"
 
 check_anagram = True
+
+if len(word_one) != len(word_two):
+    check_anagram = False
+else:
+    for x in word_one_list:
+        if word_one.count(x) != word_two.count(x):
+            check_anagram = False
+
+if check_anagram:
+    print(anagram)
+else:
+    print(not_anagram)
